4c942117da1628517a79c13884df7f7adc157ad3
Rahela Tech <Rahela.09.05@gmail.com>
2026-03-27 11:56:39 +0200

    initial commit

---
+++ app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">You did it!</h1>
+  <i class="bi bi-egg"></i>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style>
+@import "bootstrap-icons";
+@import "tailwindcss";
+</style>
